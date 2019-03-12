f143122502f52423efd5b8efce14890dce4174be
AnnaZaborovskaya <anna.zhaborovskaya@gmail.com>
2019-03-12 11:18:27 +0200

    yfhfxgx

--- AlfaKasta/js/main.js
+++ AlfaKasta/js/main.js
@@ -28,21 +28,3 @@ $(document).ready(function() {
       $(this).toggleClass('active').siblings().removeClass('active');
   });
 });
-$(document).ready(function() {
-  $('.burger').click(function(event) {
-      event.preventDefault();
-      $('.more').toggleClass('more_display').siblings().removeClass('more_display');
-  });
-});
-$(document).ready(function() {
-  $('.balance_arrow').click(function(event) {
-      event.preventDefault();
-      $('.balance_more').toggleClass('balance_more_open').siblings().removeClass('balance_more_open');
-  });
-});
-$(document).ready(function() {
-  $('.profile').click(function(event) {
-      event.preventDefault();
-      $('.profile_more').toggleClass('profile_more_open').siblings().removeClass('.profile_more_open');
-  });
-});
