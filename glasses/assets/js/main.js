ec13467e767c8396008808876c51404c0788b0a3
Racell228 <Racell228@users.noreply.github.com>
2019-05-24 10:31:57 +0300

    klhl

---
+++ glasses/assets/js/main.js
@@ -0,0 +1,14 @@
+jQuery(function(f){
+    var element = f('.btn_up');
+    f(window).scroll(function(){
+        element['fade'+ (f(this).scrollTop() > 700 ? 'In': 'Out')](100);           
+    });
+}); 
+jQuery(document).ready(function() {
+  jQuery("btn_up").click(function () {
+    elementClick = jQuery(this).attr("href")
+    destination = jQuery(elementClick).offset().top;
+    jQuery("html:not(:animated),body:not(:animated)").animate({scrollTop: destination}, 1100);
+    return false;
+  });
+});
