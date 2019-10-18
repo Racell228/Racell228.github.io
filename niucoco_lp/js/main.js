8665df5535248d7213fffbd3cac68115af0d7555
Racell228 <46070719+Racell228@users.noreply.github.com>
2019-10-18 14:35:43 +0300

    66666

--- niucoco_lp/js/main.js
+++ niucoco_lp/js/main.js
@@ -5,7 +5,14 @@ $(document).ready(function(){
     slidesToScroll: 1,
     autoplay: true,
     autoplaySpeed: 2000,
+    asNavFor: '.slider-nav',
     prevArrow: '<button type="button" class="left btn-juliet"><img src="img/shipping_arrow.png" class="left__img" alt=""></button>',
     nextArrow: '<button type="button" class="right btn-juliet"><img src="img/shipping_arrow.png"  class="right__img" alt=""></button>'
   });
+});
+$('.slider-nav').slick({
+  slidesToShow: 6,
+  slidesToScroll: 1,
+  asNavFor: '.hero_slider',
+  focusOnSelect: true
 });
