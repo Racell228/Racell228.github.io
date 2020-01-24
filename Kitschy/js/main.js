529c031a1198243bc5693ac37232c4a8ff3bf164
Юрий <46070719+Racell228@users.noreply.github.com>
2020-01-24 19:23:14 +0200

    t

--- Kitschy/js/main.js
+++ Kitschy/js/main.js
@@ -3,3 +3,30 @@ $('.mobile_burger').on('click', function(e) {
   $(this).toggleClass('menu-btn_active');
   $(".header_menu_links").toggleClass('menu_show');
 });
+
+$('.slider').slick({
+  centerMode: true,
+  centerPadding: '400rem',
+  slidesToShow: 1,
+  dots: true,
+  responsive: [
+    {
+      breakpoint: 768,
+      settings: {
+        arrows: false,
+        centerMode: true,
+        centerPadding: '40px',
+        slidesToShow: 1
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        arrows: false,
+        centerMode: true,
+        centerPadding: '40px',
+        slidesToShow: 1
+      }
+    }
+  ]
+});
