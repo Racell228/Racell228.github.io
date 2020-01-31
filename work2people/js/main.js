b365c21e30b0e413a4017bf801a7203955439d83
Racell228 <46070719+Racell228@users.noreply.github.com>
2020-01-31 11:26:56 +0200

    111111111111111111

--- work2people/js/main.js
+++ work2people/js/main.js
@@ -1,12 +1,44 @@
-  // Открываем dropdown
-  $('.dropdown').on('click', function() {
-    $(this).toggleClass('active');
+$(document).ready(function() {
+  $('.mobile_filters').click(function(event) {
+      event.preventDefault();
+      $('.filters_container').show();
+      $('body,html').addClass('hidden');
   });
+});
+$(document).ready(function() {
+  $('.close_filters').click(function(event) {
+      event.preventDefault();
+      $('.filters_container').hide();
+      $('body,html').removeClass('hidden');
+  });
+});
+$(document).ready(function() {
+  $('.job_apply').click(function(event) {
+      event.preventDefault();
+      $('.attach_overlay').show();
+      $('body,html').addClass('hidden');
+  });
+}); 
 
 
-  // Закрываем dropdown при клике по document
-  $(document).on('click', function(e) {
-    if ($(e.target).closest(".dropdown").length) return;
-    $(".dropdown").removeClass('active');
-    e.stopPropagation();
+
+
+var inputs = document.querySelectorAll('.inputfile');
+Array.prototype.forEach.call(inputs, function(input){
+  var label  = input.nextElementSibling,
+      labelVal = label.innerHTML;
+  input.addEventListener('change', function(e){
+    var fileName = '';
+    if( this.files && this.files.length > 1 )
+      fileName = ( this.getAttribute( 'data-multiple-caption' ) || '' ).replace( '{count}', this.files.length );
+    else
+      fileName = e.target.value.split( '\\' ).pop();
+    if( fileName )
+      label.querySelector( 'span' ).innerHTML = fileName;
+    else
+      label.innerHTML = labelVal;
   });
+});
+
+
+
