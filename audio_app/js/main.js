ddbaa906428c0b2be47933c4651adfeeca8595cf
Racell228 <46070719+Racell228@users.noreply.github.com>
2020-08-08 23:29:48 +0300

    audio_app tablet

--- audio_app/js/main.js
+++ audio_app/js/main.js
@@ -13,7 +13,16 @@ $(document).ready(function(){
     slidesToShow: 4,
     slidesToScroll: 1,
     prevArrow: '<button type="button" class="left_review slider_arrow"></button>',
-    nextArrow: '<button type="button" class="right_rewiew slider_arrow"></button>'
+    nextArrow: '<button type="button" class="right_rewiew slider_arrow"></button>',
+    responsive: [
+    {
+      breakpoint: 769,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 1,
+      }
+    }
+  ]
   });
 });
 $(document).ready(function() {
@@ -23,3 +32,25 @@ $(document).ready(function() {
       $(this).find('.questions_item_close').toggleClass('active');
   });
 });
+jQuery(document).ready(function() {
+  jQuery(".scroll").click(function () {
+    elementClick = jQuery(this).attr("href")
+    destination = jQuery(elementClick).offset().top;
+    jQuery("html:not(:animated),body:not(:animated)").animate({scrollTop: destination}, 1100);
+    return false;
+  });
+});
+$(document).ready(function() {
+  $('.books_item').click(function(event) {
+      event.preventDefault();
+      $('.modal').show(400);
+      $('html').addClass('html_hidden');
+  });
+}); 
+$(document).ready(function() {
+  $('.modal_close').click(function(event) {
+      event.preventDefault();
+      $('.modal').hide(400);
+      $('html').removeClass('html_hidden');
+  });
+}); 
