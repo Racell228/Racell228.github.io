f7b2b864931216fab6af15477f7bde10406cf6c6
Racell228 <46070719+Racell228@users.noreply.github.com>
2020-08-12 17:33:08 +0300

    цц

--- audio_app/js/main.js
+++ audio_app/js/main.js
@@ -44,7 +44,7 @@ $(document).ready(function(){
 $(document).ready(function() {
   $('.questions_item').click(function(event) {
       event.preventDefault();
-      $(this).find('.questions_item_answer').toggle(400);
+      $(this).find('.questions_item_answer').toggle();
       $(this).find('.questions_item_close').toggleClass('active');
   });
 });
@@ -93,3 +93,27 @@ $(document).ready(function() {
       $('html').removeClass('html_hidden');
   });
 }); 
+const boxer = boxercontainer.querySelector("img"),
+maxMove = boxercontainer.offsetWidth / 30,
+boxerCenterX = boxer.offsetLeft + (boxer.offsetWidth / 2),
+boxerCenterY = boxer.offsetTop + (boxer.offsetHeight / 2),
+fluidboxer = window.matchMedia("(min-width: 726px)");
+
+function getMousePos(xRef, yRef) {
+  
+  let panelRect = boxercontainer.getBoundingClientRect();
+  return {
+    x: Math.floor(xRef - panelRect.left) /(panelRect.right-panelRect.left)*boxercontainer.offsetWidth,
+    y: Math.floor(yRef - panelRect.top) / (panelRect.bottom -panelRect.top) * boxercontainer.offsetHeight
+    };
+}
+
+document.body.addEventListener("mousemove", function(e) {
+  let mousePos = getMousePos(e.clientX, e.clientY),
+  distX = mousePos.x - boxerCenterX,
+  distY = mousePos.y - boxerCenterY;
+  if (Math.abs(distX) < 500 && distY < 200 && fluidboxer.matches) {
+  boxer.style.transform = "translate("+(-1*distX)/12+"px,"+(-1*distY)/12+"px)";
+    boxercontainer.style.backgroundPosition = `calc(50% + ${distX/50}px) calc(50% + ${distY/50}px)`;
+  }
+})
--- busycube/js/main.js
+++ busycube/js/main.js
@@ -1,3 +1,44 @@
 $(document).ready(function() {
   $('select').styler();
 })
+$(function() {
+  var owl = $(".card_slider .owl-carousel");
+  owl.owlCarousel({
+    items: 1,
+    margin: 10,
+    dots: true,
+    nav: true
+  });
+});
+jQuery(document).ready(function($) {
+  let outs = document.querySelectorAll('.card_slider');
+  for (let out of outs) {
+      let owl = out.querySelector('.owl-carousel');
+      let btns = out.querySelectorAll(':scope .card_slider_nav_btn');
+      let owlJq = $(owl);
+      owlJq.owlCarousel({
+          items: 1,
+          loop: false,
+          margin: 10,
+          nav: false,
+          dots: false,
+      });
+      for (let btn of btns) {
+          btn.addEventListener('click', function () {
+              owlJq.trigger('to.owl.carousel', [$(this).index()]);
+          })
+      }
+  };
+
+});
+$(function() {
+  var owl = $(".reviews_slider .owl-carousel");
+  owl.owlCarousel({
+    items: 2,
+    margin: 30,
+    dots: false,
+    nav: true,
+    center:true,
+    loop: true
+  });
+});
