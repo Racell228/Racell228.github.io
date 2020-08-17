d32d02303b4b0c48151eaacbe5793f61e2aa8d05
Racell228 <46070719+Racell228@users.noreply.github.com>
2020-08-17 14:12:08 +0300

    uuu

--- busycube/js/main.js
+++ busycube/js/main.js
@@ -34,11 +34,63 @@ jQuery(document).ready(function($) {
 $(function() {
   var owl = $(".reviews_slider .owl-carousel");
   owl.owlCarousel({
-    items: 2,
-    margin: 30,
-    dots: false,
-    nav: true,
-    center:true,
-    loop: true
+    responsive : {
+    // breakpoint from 0 up
+    0 : {
+        items: 1,
+        margin: 30,
+        dots: false,
+        nav: true,
+        center:true,
+        loop: true,
+    },
+    769 : {
+       items: 2,
+       center:false,
+       items: 2,
+      margin: 30,
+      dots: false,
+      nav: true,
+      center:true,
+      loop: true,
+    }
+  }
   });
 });
+$(document).ready(function() {
+  $('.overview_btn').click(function(event) {
+      event.preventDefault();
+      $('.modal_form').show();
+  });
+});  
+$(document).ready(function() {
+  $('.gift_btn').click(function(event) {
+      event.preventDefault();
+      $('.modal_exit').show();
+  });
+});  
+$(document).ready(function() {
+  $('.close_btn').click(function(event) {
+      event.preventDefault();
+      $('.modal_form,.modal_exit').hide();
+  });
+});  
+// var overlay = document.getElementById('overlay');
+// var vid = document.getElementById('video');
+
+// if(overlay.addEventListener){
+//     overlay.addEventListener("click", play, false)
+//   }else if(overlay.attachEvent){
+//     overlay.attachEvent("onclick", play)
+//   }
+
+// function play() { 
+//     if (vid.paused){
+//         vid.play(); 
+//         overlay.className = "o";
+//     }else {
+//         vid.pause(); 
+//         overlay.className = "";
+//     }
+// } 
+
