be7a3a92d52ec6abde31c41f743704788b7a433e
Racell228 <46070719+Racell228@users.noreply.github.com>
2020-09-07 20:34:09 +0300

    wwwww

--- bimbabo/js/main.js
+++ bimbabo/js/main.js
@@ -1,3 +1,16 @@
 $(document).ready(function() {
   $('select').styler();
-})
+});
+$(document).ready(function(){
+  $('.subheader_slider').slick({
+    infinite: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    dots: true,
+    arrows:true,
+    prevArrow: '<button type="button" class="left slider_arrow"></button>',
+    nextArrow: '<button type="button" class="right slider_arrow"></button>',
+    // autoplay:true,
+    // autoplaySpeed: 10000,
+  });
+});
