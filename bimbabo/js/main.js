fe13f3014e873daca7110450f3e9f7517ac39154
Racell228 <46070719+Racell228@users.noreply.github.com>
2020-09-21 13:18:08 +0300

    www

--- bimbabo/js/main.js
+++ bimbabo/js/main.js
@@ -30,3 +30,95 @@ $(document).ready(function() {
       $(id1).fadeIn(0).siblings(0).fadeOut(0);
   });
 });
+$(document).ready(function() {
+  $('.products_overlay_tab:first-child').show();
+  $('.products_nav_links_item').click(function(event) {
+      $(this).addClass('active').siblings().removeClass('active');
+      var id1 = $(this).attr('href');
+      $(id1).fadeIn(0).siblings(0).fadeOut(0);
+  });
+});
+
+
+
+$(document).ready(function() {
+  $('.category_body_filters_item_name').click(function(event) {
+      $(this).parent().find('.category_body_filters_item_inputs').toggle(400);
+      $(this).toggleClass('rotate_arrow');
+  });
+});
+
+
+
+$(function() {
+
+  (function quantityProducts() {
+    var $quantityArrowMinus = $(".quantity-arrow-minus");
+    var $quantityArrowPlus = $(".quantity-arrow-plus");
+    var $quantityNum = $(".quantity-num");
+
+    $quantityArrowMinus.click(quantityMinus);
+    $quantityArrowPlus.click(quantityPlus);
+
+    function quantityMinus() {
+      if ($quantityNum.val() > 1) {
+        $quantityNum.val(+$quantityNum.val() - 1);
+      }
+    }
+
+    function quantityPlus() {
+      $quantityNum.val(+$quantityNum.val() + 1);
+    }
+  })();
+
+});
+
+$(document).ready(function() {
+  $('.products_item_fast_buy').click(function(event) {
+      event.preventDefault();
+      $('.fastbuy').show(400);
+      $('html,body').addClass('hidden');
+  });
+});  
+$(document).ready(function() {
+  $('.modal_close').click(function(event) {
+      event.preventDefault();
+     $('.fastbuy').hide(400);
+    $('html,body').removeClass('hidden');
+  });
+});  
+
+
+
+$(function() {
+  var owl = $(".single_slider .owl-carousel");
+  owl.owlCarousel({
+    items: 1,
+    margin: 10,
+    dots: true,
+    nav: true,
+  });
+});
+jQuery(document).ready(function($) {
+  let outs = document.querySelectorAll('.single_slider');
+  for (let out of outs) {
+      let owl = out.querySelector('.owl-carousel');
+      let btns = out.querySelectorAll(':scope .card_slider_nav_btn');
+      let owlJq = $(owl);
+      owlJq.owlCarousel({
+          items: 1,
+          loop: false,
+          margin: 10,
+          nav: false,
+          dots: false,
+      });
+      for (let btn of btns) {
+          btn.addEventListener('click', function () {
+              owlJq.trigger('to.owl.carousel', [$(this).index()]);
+              $('.card_slider_nav_btn').removeClass('active')
+              $(this).addClass('active');
+          })
+      }
+  };
+
+});
