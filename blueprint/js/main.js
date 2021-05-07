c3acc0973a1cecba160c7014eb04ca1c5096819f
Racell228 <46070719+Racell228@users.noreply.github.com>
2021-05-07 19:48:36 +0300

    Blueprint

---
+++ blueprint/js/main.js
@@ -0,0 +1,18 @@
+$(document).ready(function(){
+  $('.slider').slick({
+    slidesToShow:1,
+    slidesToScroll: 1,
+    prevArrow: '<button type="button" class="slider_btn left"></button>',
+    nextArrow: '<button type="button" class="slider_btn right"></button>',
+    dots: false,
+  //   responsive: [
+  //   {
+  //     breakpoint: 481,
+  //     settings: {
+  //       slidesToShow: 1,
+  //       slidesToScroll: 1,
+  //     }
+  //   }
+  // ]
+  });
+});
