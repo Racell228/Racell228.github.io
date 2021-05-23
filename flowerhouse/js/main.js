b8207a2ce9ea7bd31259c7bf1cbe3ca753c426ef
Racell228 <46070719+Racell228@users.noreply.github.com>
2021-05-23 19:41:14 +0300

    flowerhouse

--- flowerhouse/js/main.js
+++ flowerhouse/js/main.js
@@ -7,3 +7,34 @@ $(document).ready(function(){
     dots: true
   });
 });
+$(document).ready(function(){
+  $('.single_slider').slick({
+    infinite: true,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    arrows: true,
+    dots: false,
+    prevArrow:'<button class="prev"></button>',
+  	nextArrow:'<button class="next"></button>',
+  	variableWidth: true
+  });
+});
+$(document).ready(function() {
+  $('.header_nav_close').click(function(event) {
+     $('.header_nav').hide(400);
+  });
+});
+$(document).ready(function() {
+  $('.header_burger').click(function(event) {
+     $('.header_nav').show(400);
+  });
+});
+
+if(window.matchMedia('(max-width: 480px)').matches){
+  	$(document).ready(function() {
+	  	$('.header_phone_item').click(function(event) {
+	      event.preventDefault();
+	      
+	  	});
+	});
+}
