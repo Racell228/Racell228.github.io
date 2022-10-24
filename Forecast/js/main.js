a0102a54e4b0c106d1cb7765d8dcfa3326fb0a44
Racell228 <46070719+Racell228@users.noreply.github.com>
2022-10-24 18:24:54 +0300

    x

--- Forecast/js/main.js
+++ Forecast/js/main.js
@@ -5,12 +5,32 @@ $(document).ready(function() {
       $(this).find('.target_item_black_item_title img').toggleClass('active');
   });
 });
-//Переключение навыков(О нас)
 $(document).ready(function() {
   $('.target_nav_link').click(function(event) {
     event.preventDefault();
       $(this).addClass('active').siblings().removeClass('active');
       var id1 = $(this).attr('href');
       $(id1).fadeIn(300).siblings(0).fadeOut(0);
+  });
+});
+$(document).ready(function() {
+  $('.header_burger').click(function(event) {
+      event.preventDefault();
+      $(this).toggleClass('active');
+      $('.menu').toggle(400);
+      $('html,body').toggleClass('overflow');
+  });
+});
+
+
+$(document).ready(function() {
+  $('.menu_item').click(function(){
+    $('.header_burger').removeClass('active');
+    $('.menu').hide(400);
+    $('html,body').removeClass('overflow');
+    var el = $(this).attr('href');
+    $('html,body').animate({
+      scrollTop: $(el).offset().top}, 1000);
+    return false;
   });
 });
