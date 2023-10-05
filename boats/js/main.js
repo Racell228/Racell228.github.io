45ac2d3e51b4d2439265f0e268deadca82cf2f01
Racell228 <ignatenko.ura000000@gmail.com>
2023-10-05 15:41:45 +0300

    boats

---
+++ boats/js/main.js
@@ -0,0 +1,29 @@
+$(document).ready(function(){
+	$('.hero_slider').on('init reInit afterChange', function (event, slick, currentSlide, nextSlide) {
+	  if(!slick.$dots){
+	  	return;
+	  }
+	  var i = (currentSlide ? currentSlide : 0) + 1;
+	   $('.slider_count').text(i + '/' + (slick.$dots[0].children.length));
+	});
+  $('.hero_slider').slick({
+    infinite: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    dots: true,
+    arrows:true,
+    prevArrow: '<button type="button" class="left"></button>',
+    nextArrow: '<button type="button" class="right"></button>',
+  });
+});
+$(document).ready(function() {
+  $('select').styler();
+});
+
+
+$(document).ready(function() {
+  $('.filter_item_title').click(function(event) {
+      $(this).toggleClass('active');
+      $(this).parent().find('.filter_item_overlay').toggle(400)
+  });
+});
